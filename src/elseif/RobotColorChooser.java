3261340caa126dc757070aab5d760e63564c0497
Rohan890 <league@WTS7.attlocal.net>
2018-09-02 12:56:49 -0700

    Partially finished

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,52 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot r2d2 = new Robot();
+		// 3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 100; i++) {
+			String color = JOptionPane.showInputDialog("choose a color Options are red, blue, and Green.");
 
-        //6. If the user doesn’t enter anything, choose a random color
+			if (color.equals("red")) {
+           r2d2setPenColor(Color.red);
+           r2d2.setPenWidth(10);
+           
+			}
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+			if (color.equals("blue")) {
+				r2d2setPenColor(Color.blue);
+				 r2d2.setPenWidth(10);
+				
+			}
+			if (color.equals("green")) {
+				r2d2setPenColor(Color.green);
+				 r2d2.setPenWidth(10);
+			}
+
+		}
+
+		// 5. Use an if/else statement to set the pen color that the user requested
+
+		// 6. If the user doesn’t enter anything, choose a random color
+
+		// 7. Put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+		// 4. Set the pen width to 10
+
+		// 2. Make the robot draw a shape (this will take more than one line of code)
+
+	}
+
+	private static void r2d2setPenColor(Color red) {
+		// TODO Auto-generated method stub
 		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
 	}
 }
