245da2bed44c66ba8306bd9a21cf9e5f0aa8c40f
aryanmshah <league@WTS22.attlocal.net>
2018-09-15 11:28:04 -0700

    hi

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,32 +5,59 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
-		// 2. Print out the random variable above
-		
-		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(100) + 1;
 
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+		// 2. Print out the random variable above
+
+		// 11. Repeat steps 1 to 10 ten times
+		String guess = JOptionPane.showInputDialog("Guess");
+		System.out.println(random);
+
+		// 1. Ask the user for a guess using a pop-up window, and save their response
+
+		// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+		int num = Integer.parseInt(guess);
+
+		// 5. if the guess is correct
+		if (num == random) {
+			JOptionPane.showConfirmDialog(null, "Your guess is correct u win");
+
+			System.exit(0);
+
+		}
+		// 6. Win
+		// 12. Use "System.exit(0);" to quit the game if the user guessed the right
+		// answer.
+		// 7. if the guess is high
+		if (num > random) {
+			JOptionPane.showConfirmDialog(null, "Your guess is too high");
+
+		}
+		// 8. Tell them it's too high
+		// 9. if the guess is low
+		// 10. Tell them it's too low
+		if (num > random) {
+			JOptionPane.showConfirmDialog(null, "Your guess is too low");
+		}
 
 		// 13. Tell them they lose
-		
+
+	}
+
+	private static void random() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void Random() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
-
-
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -3,53 +3,67 @@
 
 package elseif;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
-
-import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		// 1. Create a new Robot
 		Robot r2d2 = new Robot();
+		r2d2.penDown();
 		// 3. Ask the user what color they would like the robot to draw
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 4; i++) {
 			String color = JOptionPane.showInputDialog("choose a color Options are red, blue, and Green.");
 
 			if (color.equals("red")) {
-           r2d2setPenColor(Color.red);
-           r2d2.setPenWidth(10);
-           
+				r2d2.setPenColor(Color.red);
+				r2d2.setPenWidth(10);
+
 			}
 
 			if (color.equals("blue")) {
-				r2d2setPenColor(Color.blue);
-				 r2d2.setPenWidth(10);
-				
+				r2d2.setPenColor(Color.blue);
+				r2d2.setPenWidth(10);
+
 			}
 			if (color.equals("green")) {
-				r2d2setPenColor(Color.green);
-				 r2d2.setPenWidth(10);
+				r2d2.setPenColor(Color.green);
+				r2d2.setPenWidth(10);
 			}
 
+			if (color.equals("")) {
+				r2d2.setRandomPenColor();
+				r2d2.setPenWidth(10);
+			}
+
+			r2d2.move(50);
+			r2d2.turn(90);
+			r2d2.move(50);
+			r2d2.turn(90);
+			r2d2.move(50);
+			r2d2.turn(90);
+			r2d2.move(50);
+			r2d2.turn(90);
+
+			// 5. Use an if/else statement to set the pen color that the user requested
+
+			// 6. If the user doesn’t enter anything, choose a random color
+
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
+
+			// 4. Set the pen width to 10
+
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+
 		}
-
-		// 5. Use an if/else statement to set the pen color that the user requested
-
-		// 6. If the user doesn’t enter anything, choose a random color
-
-		// 7. Put a loop around your code so that you keep asking the user for more
-		// colors & drawing them
-
-		// 4. Set the pen width to 10
-
-		// 2. Make the robot draw a shape (this will take more than one line of code)
-
 	}
 
 	private static void r2d2setPenColor(Color red) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
